5e8fddca5fd68f273e9584d0bf547291f16d7a3d
Abuzar Bagewadi <55507269+abuzarbagewadi@users.noreply.github.com>
2021-08-07 22:59:59 +0530

    added env file

--- app.js
+++ app.js
@@ -35,16 +35,23 @@ app.post("/", function(req,res){
 
     const jsonData = JSON.stringify(data);
 
-    const url = "https://us5.api.mailchimp.com/3.0/lists/fd300966ff";
+    const url = URL;
 
     const options = {
         method: "POST",
-        auth: "stealth:ea003b6f94c6237a554a6e85293e656a-us5"
+        auth: AUTH_TOKEN
     }
 
     const request = https.request(url, options, function(response){
+
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html");
+        }
             response.on("data", function(data){
-                console.log(JSON.parse(data));
+                console.log(response.statusCode);
             })
     })
     
@@ -52,7 +59,11 @@ app.post("/", function(req,res){
     request.end();
 });
 
-app.listen(3000, function(){
+app.post("/failure", function(req,res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000");
 })
 
